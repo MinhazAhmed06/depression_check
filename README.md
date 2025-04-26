b72958be23832fdee5c8f5d5b73ac265ccd233c7
unknown <minhaz.ahmed000@gmail.com>
2025-04-26 21:59:19 +0530

    continuous pipeline

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ zenml integration install mlflow -y
 zenml experiment-tracker register mlflow_tracker --flavor=mlflow
 zenml model-deployer register mlflow --flavor=mlflow
 zenml stack register mlflow_stack -a default -o default -d mlflow -e mlflow_tracker --set
+
+mlflow ui --backend-store-uri "[given_uri]"
---
+++ pipelines/deployment_pipeline.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+from zenml import pipeline, step
+from zenml.config import DockerSettings
+from zenml.constants import DEFAULT_SERVICE_START_STOP_TIMEOUT
+from zenml.integrations.constants import MLFLOW
+from zenml.integrations.mlflow.model_deployers.mlflow_model_deployer import MLFlowModelDeployer
+from zenml.integrations.mlflow.services import mlflow_deployment
+from zenml.integrations.mlflow.steps import mlflow_model_deployer_step
+from pydantic import BaseModel
+
+from steps.ingest_data import ingest_df
+from steps.clean_data import clean_df
+from steps.model_train import train_model
+from steps.evaluation import evaluate_model
+from steps.config import ModelNameConfig
+
+docker_settings = DockerSettings(required_integrations=[MLFLOW])
+
+class DeploymentTriggerConfig(BaseModel):
+    min_accuracy: float = 0.92
+
+@step
+def deployment_trigger(
+    accuracy: float,
+    config: DeploymentTriggerConfig,
+):
+    return accuracy >= config.min_accuracy
+
+@pipeline(enable_cache=False, settings = {"docker":docker_settings})
+def continuous_deployment_pipeline(
+    data_path: str,
+    min_accuracy: float = 0.92,
+    workers: int = 1,
+    timeout: int = DEFAULT_SERVICE_START_STOP_TIMEOUT,
+):
+    df = ingest_df(data_path=data_path)
+    X_train, X_test, y_train, y_test = clean_df(df)
+    model_config = ModelNameConfig()
+    model = train_model(X_train, y_train, model_config)
+    mse, r2, rmse = evaluate_model(model, X_test, y_test)
+    dtconfig = DeploymentTriggerConfig()
+    deploy_decision = deployment_trigger(r2, dtconfig)
+    mlflow_model_deployer_step(
+        model = model,
+        deploy_decision = deploy_decision,
+        workers = workers,
+        timeout = timeout,
+    )
